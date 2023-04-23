421018265f28b87b5cebf443932263f3821e0fc4
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-04-23 15:03:05 +0700

    Up date day 4

--- Day4/day4.js
+++ Day4/day4.js
@@ -10,6 +10,8 @@
 // });
 // console.log("Name dc lấy từ localStorage: " + nameFromLocalStorage);
 
+let inputName = document.querySelector("#inputName");
+
 let student = {
   id: 1,
   name: "Phương",
@@ -26,5 +28,14 @@ console.log(student);
 // Nguyên tắc: Khi set Value vào trong localStorage nếu dữ liệu chúng ta lưu là dạng Oject hoặc array => Phải dùng dùng JSON.stringify() giá trị đó trước
 // rồi mới lưu vào localStorage
 localStorage.setItem("StudentOfMindx", JSON.stringify(student));
-let getValue = localStorage.getItem("StudentOfMindx");
-console.log("Value = " + getValue);
+
+let btnSave = document.querySelector("button");
+btnSave.addEventListener("click", function () {
+  // let getValue = localStorage.getItem("StudentOfMindx"); // => JSON.parse(localStorage.getItem("StudentOfMindx"))
+  let personLocalStorage = JSON.parse(localStorage.getItem("StudentOfMindx")); // => ĐỐi tương
+  console.log("Value từ input = " + inputName.value);
+  personLocalStorage.name = inputName.value;
+
+  localStorage.setItem("StudentOfMindx", JSON.stringify(personLocalStorage));
+});
+// string -> object: JSON.parse()
