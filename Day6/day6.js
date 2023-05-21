4004ac21aa73d406b356d49ec84d0535a3708197
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-05-21 15:33:36 +0700

    update menu

--- Day6/day6.js
+++ Day6/day6.js
@@ -6,3 +6,13 @@ let phepToan = "88 x 66";
 let tachTuString = phepToan.split(" ");
 
 console.log(tachTuString);
+
+let moreBTN = document.querySelector(".more");
+let toggle = document.querySelector(".toggle");
+moreBTN.addEventListener("mouseover", function () {
+  toggle.style.backgroundColor = "blue";
+});
+
+moreBTN.addEventListener("mouseout", function () {
+  toggle.style.backgroundColor = "red";
+});
