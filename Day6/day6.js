51fb2e164e8b1e225b3723037d8a6a0fb572ef32
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-05-21 14:49:56 +0700

    update menu

---
+++ Day6/day6.js
@@ -0,0 +1,8 @@
+// let name = "Nguyễn Danh Phương";
+// let splitFunction = name.split("");
+// console.log(splitFunction);
+
+let phepToan = "88 x 66";
+let tachTuString = phepToan.split(" ");
+
+console.log(tachTuString);
