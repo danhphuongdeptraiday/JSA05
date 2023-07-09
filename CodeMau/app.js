5292e2a191ad55a011ae61d3427dbe66d2beb9c9
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-07-09 15:47:32 +0700

    Vinh Problem

--- CodeMau/app.js
+++ CodeMau/app.js
@@ -39,18 +39,32 @@ if (getListCart == null) {
 } else {
   for (let i = 0; i < listButtonInProduct.length; i++) {
     listButtonInProduct[i].addEventListener("click", function () {
+      console.log(
+        listButtonInProduct[i].previousElementSibling.previousElementSibling.src
+      );
       console.log(listButtonInProduct[i].parentElement.getAttribute("id"));
       let id = listButtonInProduct[i].parentElement.getAttribute("id");
       getListCart = JSON.parse(localStorage.getItem("listCart"));
       getListCart.push(id);
       localStorage.setItem("listCart", JSON.stringify(getListCart));
 
+      //
+      let imgSrc =
+        listButtonInProduct[i].previousElementSibling.previousElementSibling
+          .src;
+      //
       let newDiv = document.createElement("div");
       newDiv.className = "item_at_cart";
-      newDiv.innerHTML = `<h2>${listButtonInProduct[i].previousElementSibling.innerText}</h2>
-        <div class="deleteButon">X</div>
+      newDiv.innerHTML = `
+        <img src=${imgSrc}/>
+        <h2>${listButtonInProduct[i].previousElementSibling.innerText}</h2>
+        <div class="deleteButton" onClick="deleteBtn(this)">X</div>
       `;
       cartContainer.append(newDiv);
     });
   }
+}
+
+function deleteBtn(e) {
+  e.parentElement.remove();
 }
